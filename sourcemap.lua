3b9cf161577a6dd0d70013e368efed6899c8d371
hourfrozen <hourfrozen4@gmail.com>
2026-01-26 21:02:50 -0500

    tweak sourcemap generation

--- sourcemap.lua
+++ sourcemap.lua
@@ -17,6 +17,29 @@ function src.init()
 	modules = helper.generateInstance("Folder")
 	modules.Name = "Modules"
 	modules.Parent = root
+	-- breaking my own rules..
+	-- only acceptable instances of instance.new being used in a sourcemap, crucify me if you will idc.
+	local contained = Instance.new("Frame", res)
+	contained.Name = "contained"
+	local warnbox = Instance.new("TextLabel", contained)
+	warnbox.Name = "warning"
+	local textbox = Instance.new("TextBox", contained)
+	textbox.Name = "INPUT"
+	contained.Size = UDim2.new(1, 0, 1, 0)
+	warnbox.Text = "To use pkg, you must first enter your github token:"
+	warnbox.TextScaled = true
+	warnbox.Size = UDim2.new(1, 0, 0.1, 0)
+	warnbox.Position = UDim2.new(0, 0, 1, 0)
+	warnbox.AnchorPoint = Vector2.new(0, 1)
+	warnbox.TextColor3 = Color3.new(1, 1, 1)
+	warnbox.BackgroundColor3 = Color3.new(0, 0, 0)
+	warnbox.BackgroundTransparency = 0.7
+	contained.BackgroundColor3 = Color3.new(0, 0, 0)
+	contained.BackgroundTransparency = 0.8
+	textbox.PlaceholderText = "Type Here.."
+	textbox.Size = UDim2.new(0.4, 0, 0.1, 0)
+	textbox.Position = UDim2.new(0.5, 0, 0.5, 0)
+	textbox.AnchorPoint = Vector2.new(0.5, 0.5)
 end
 
 src.mapped = {
@@ -61,6 +84,12 @@ src.mapped = {
 		type = "ModuleScript",
 		destination = modules,
 		name = "tools",
+	},
+	{
+		path = "src/Modules/RepositoryCoreModules/static/sourcemapref.lua",
+		type = "ModuleScript",
+		destination = res,
+		name = "sourcemapref",
 	},
 }
 
